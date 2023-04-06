f5c8206583395fb44f82451d842e08d9f5a69cef
Antonio lu <antonioraylu@outlook.com>
2023-04-06 22:22:07 +1000

    JS update: Creat function to obtain data and store it in the local storage

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+
+var cityname = $('#City')
+var cityTemp = $('#temperature')
+var cityHumidity = $('#humidity')
+var cityWindSpeed = $('#wind-speed')
+var dateDisplay = $('#date')
+var miniDateDisplay = $('.mini-date')
+
+
+var inputBtn = $('#button-addon2')
+var inputContainer = $('.input-group')
+var cityList = $('#city-display-list')
+var firstBlock = $('.first-block')
+
+
+
+inputContainer.on('click', '#button-addon2', function(event){
+    
+    var displayCityAnchor = $('<a>');
+    displayCityAnchor.addClass('list-group-item list-group-item-action list-group-item-dark city');
+    
+    var value = $(event.target).parent('div').children('input').val().trim().toLowerCase()
+    if(value == ""){
+        return
+    } 
+
+    if(containsWhitespace(value)){
+        const words = value.split(" ");
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        }
+        var CapitalizedWord = words.join(" ");
+    } else {
+        var CapitalizedWord = value.substring(0, 1).toUpperCase() + value.substring(1);
+    }
+
+    $.getJSON('cities.json', function(data) {
+        $.each(data, function(key, val) {
+            if(val.name == CapitalizedWord){
+                let CityList = JSON.parse(localStorage.getItem("City Collection"));
+                $.each(  CityList, function( i, val ){
+                    if(val == CapitalizedWord){
+                        CityList.splice(i, 1)
+                        return
+                    }
+                });
+                correctspelling = true
+                $('#input-city').val('')
+                $('.alert').text('')
+                return
+            }
+            if(correctspelling == false){
+                $('.alert').text('Invalid Input')
+                return
+            }
+         })
+    });
+    correctspelling = false
+})
+
+
+var cityListArray = []
+
+function storeValue(input){
+    let storedCities = JSON.parse(localStorage.getItem("City Collection"));
+
+    if(storedCities !== null){
+        cityListArray = storedCities
+    } 
+
+    $.each(  cityListArray, function( i, val ){
+        if(val == input){
+            cityListArray.splice(i, 1)
+            return
+        }
+      });
+
+    cityListArray.push(input);
+    localStorage.setItem('City Collection', JSON.stringify(cityListArray))
+
+
+}
+
+function containsWhitespace(str) {
+    return str.includes(' ');
+  }
+  
+
